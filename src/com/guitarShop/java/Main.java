93aa4e6027340cabd623e045b4a2246166b70916
wohajo <michalos222@gmail.com>
2020-05-11 14:14:40 +0200

    First works

---
+++ src/com/guitarShop/java/Main.java
@@ -0,0 +1,27 @@
+package com.guitarShop.java;
+
+import com.guitarShop.java.controllers.Controller;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/guitarShop/resources/login.fxml"));
+        Parent root = (Parent) loader.load();
+        Controller controller = (Controller) loader.getController();
+        primaryStage.setTitle("Guitar Shop");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        controller.setStage(primaryStage);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/com/guitarShop/java/controllers/Controller.java
@@ -0,0 +1,28 @@
+package com.guitarShop.java.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller {
+
+    Stage primaryStage;
+
+    public Controller() {
+
+    }
+
+    public void setStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    @FXML
+    private void login() throws IOException {
+        Parent loggedIn = FXMLLoader.load(getClass().getResource("/com/guitarShop/resources/loggedIn.fxml"));
+        this.primaryStage.setScene(new Scene(loggedIn));
+    }
+}
