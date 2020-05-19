1843747cb68db5d7ecc6b2e48528fae15a98b7c4
wohajo <michalos222@gmail.com>
2020-05-19 23:38:51 +0200

    some table tests

---
+++ src/com/guitarShop/java/controllers/Guitar.java
@@ -0,0 +1,118 @@
+package com.guitarShop.java.controllers;
+
+public class Guitar {
+
+    private int guitarID;
+    private String manufacturer;
+    private int guitarPrice;
+    private String guitarType;
+    private String pickupsType;
+    private String bridgeType;
+    private Boolean lockingTuners;
+    private int numberOfGuitars;
+    private String model;
+    private String modelDescription;
+    private int numberOfStrings;
+
+    public Guitar(int guitarID, String manufacturer, int guitarPrice, String guitarType, String pickupsType, String bridgeType, Boolean lockingTuners, int numberOfGuitars, String model, String modelDescription, int numberOfStrings) {
+        this.guitarID = guitarID;
+        this.manufacturer = manufacturer;
+        this.guitarPrice = guitarPrice;
+        this.guitarType = guitarType;
+        this.pickupsType = pickupsType;
+        this.bridgeType = bridgeType;
+        this.lockingTuners = lockingTuners;
+        this.numberOfGuitars = numberOfGuitars;
+        this.model = model;
+        this.modelDescription = modelDescription;
+        this.numberOfStrings = numberOfStrings;
+    }
+
+    public int getGuitarID() {
+        return guitarID;
+    }
+
+    public void setGuitarID(int guitarID) {
+        this.guitarID = guitarID;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getGuitarPrice() {
+        return guitarPrice;
+    }
+
+    public void setGuitarPrice(int guitarPrice) {
+        this.guitarPrice = guitarPrice;
+    }
+
+    public String getGuitarType() {
+        return guitarType;
+    }
+
+    public void setGuitarType(String guitarType) {
+        this.guitarType = guitarType;
+    }
+
+    public String getPickupsType() {
+        return pickupsType;
+    }
+
+    public void setPickupsType(String pickupsType) {
+        this.pickupsType = pickupsType;
+    }
+
+    public String getBridgeType() {
+        return bridgeType;
+    }
+
+    public void setBridgeType(String bridgeType) {
+        this.bridgeType = bridgeType;
+    }
+
+    public Boolean getLockingTuners() {
+        return lockingTuners;
+    }
+
+    public void setLockingTuners(Boolean lockingTuners) {
+        this.lockingTuners = lockingTuners;
+    }
+
+    public int getNumberOfGuitars() {
+        return numberOfGuitars;
+    }
+
+    public void setNumberOfGuitars(int numberOfGuitars) {
+        this.numberOfGuitars = numberOfGuitars;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModelDescription() {
+        return modelDescription;
+    }
+
+    public void setModelDescription(String modelDescription) {
+        this.modelDescription = modelDescription;
+    }
+
+    public int getNumberOfStrings() {
+        return numberOfStrings;
+    }
+
+    public void setNumberOfStrings(int numberOfStrings) {
+        this.numberOfStrings = numberOfStrings;
+    }
+}
--- src/com/guitarShop/java/controllers/tabControllers/StockTabController.java
+++ src/com/guitarShop/java/controllers/tabControllers/StockTabController.java
@@ -1,18 +1,68 @@
 package com.guitarShop.java.controllers.tabControllers;
 
-
+import com.guitarShop.java.controllers.Guitar;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class StockTabController {
 
-    @FXML TableView stockTable;
+    @FXML TableView<Guitar> stockTable;
     @FXML Button refreshStockTableButton;
+
+    private void initializeTable() {
+
+        ObservableList<Guitar> data = FXCollections.observableArrayList();
+
+        Guitar guitar = new Guitar(1, "fender", 2500, "strat", "sss",
+                "yes", true, 6, "aaa", "aaaaaa", 4);
+
+        data.add(guitar);
+
+        TableColumn<Guitar, String> manufacturerCol = new TableColumn<>("Maufacturer");
+        manufacturerCol.setMinWidth(200);
+        manufacturerCol.setCellValueFactory(new PropertyValueFactory<Guitar, String>("manufacturer"));
+
+        TableColumn<Guitar, String> guitarModelCol = new TableColumn<>("Model");
+        guitarModelCol.setMinWidth(200);
+        guitarModelCol.setCellValueFactory(new PropertyValueFactory<Guitar, String>("model"));
+
+//        TableColumn priceColumn = new TableColumn("Price");
+//        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+//
+//        TableColumn guitarTypeCol = new TableColumn("Type");
+//        guitarTypeCol.setCellValueFactory(new PropertyValueFactory<>("guitarType"));
+//
+//        TableColumn pickupsCol = new TableColumn("Pickups");
+//        pickupsCol.setCellValueFactory(new PropertyValueFactory<>("pickupsType"));
+//
+//        TableColumn bridgeCol = new TableColumn("Bridge");
+//        bridgeCol.setCellValueFactory(new PropertyValueFactory<>("bridgeType"));
+//
+//        TableColumn tunersCol = new TableColumn("Tuners");
+//        priceColumn.setCellValueFactory(new PropertyValueFactory<>("lockingTuners"));
+//
+//        TableColumn inStockCol = new TableColumn("In Stock");
+//        priceColumn.setCellValueFactory(new PropertyValueFactory<>("numberOfGuitars"));
+//
+//        TableColumn stringsCol = new TableColumn("Strings");
+//        priceColumn.setCellValueFactory(new PropertyValueFactory<>("numberOfStrings"));
+
+        this.stockTable = new TableView<>();
+        this.stockTable.setItems(data);
+        stockTable.getColumns().addAll(manufacturerCol, guitarModelCol);
+//        stockTable.getColumns().addAll(manufacturerCol, guitarModelCol, priceColumn, guitarTypeCol, pickupsCol,
+//                bridgeCol, tunersCol, inStockCol, stringsCol);
+    }
 
     @FXML
     private void refreshStockTable() {
+        initializeTable();
 
+        System.out.println();
     }
-
 }
